6246428ce3d32c519441018791c01209c86410b8
Abdi-Isse <abdi.isse96@gmail.com>
2021-05-09 13:32:10 +0800

    [WORK IN PROGRESS] Add about page

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from website import db, create_app, models
 import os
-
 app = create_app()
 with app.app_context():
     db.create_all()
@@ -8,7 +7,7 @@ with app.app_context():
 if __name__ == '__main__':
     # code for mac
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
     # code for Linux
     # app.run(debug=True)
