1e1204b659163c77bf08bd746b406f4915ff6f77
Farshad Ghanbari <farshad.ghanbari@hotmail.com>
2021-05-16 16:49:23 +0800

    muscle groups content and sample quiz

--- website/main.py
+++ website/main.py
@@ -26,6 +26,11 @@ def home():
 def courses():
     return render_template('courses.html')
 
+@main.route("/muscle")
+@login_required
+def muscle():
+    return render_template('muscle.html')    
+
 @main.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
